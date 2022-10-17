4a79cb0f563758ef8c7e7d03bb81ee56f411adc8
leslieshea <lshea@audigy.com>
2022-10-17 15:18:09 -0700

    Changed to Commonjs syntax

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,42 +1,44 @@
-import webpack from "webpack";
-import { resolve } from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 process.env.NODE_ENV = "development";
 
-export const mode = "development";
-export const target = "web";
-export const devtool = "cheap-module-source-map";
-export const entry = "./src/index";
-export const output = {
-	path: resolve(__dirname, "build"),
-	publicPath: "/",
-	filename: "bundle.js"
-};
-export const devServer = {
-	stats: "minimal",
-	overlay: true,
-	historyApiFallback: true,
-	disableHostCheck: true,
-	headers: { "Access-Control-Allow-Origin": "*" },
-	https: false
-};
-export const plugins = [
-	new HtmlWebpackPlugin({
-		template: "src/index.html",
-		favicon: "src/favicon.ico"
-	})
-];
-export const module = {
-	rules: [
-		{
-			test: /\.(js|jsx)$/,
-			exclude: /node_modules/,
-			use: ["babel-loader", "eslint-loader"]
-		},
-		{
-			test: /(\.css)$/,
-			use: ["style-loader", "css-loader"]
-		}
-	]
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map",
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
 };
