e81ea0ad68a544ddaf6fda5ad5692cc7d59def35
leslieshea <lshea@audigy.com>
2022-10-28 15:03:55 -0700

    Removed hard-coded strings and created action types constants

--- src/redux/reducers/courseReducers.js
+++ src/redux/reducers/courseReducers.js
@@ -1,6 +1,8 @@
+import * as types from '../actions/actionTypes';
+
 export default function courseReducer(state = [], action) {
 	switch(action.type) {
-		case "CREATE_COURSE":
+		case types.CREATE_COURSE:
 			return [ ...state, { ...action.course }]; //this will create a new array with all existing courses + new course passed in
 		default:
 			return state;
